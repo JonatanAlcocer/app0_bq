2a7f48e77739dc5255099ce3b022c72fb94f72d4
Jonatan Alcocer <jony.al.90@gmail.com>
2014-02-13 00:24:38 +0100

    modified:   AppDropboxBq/AndroidManifest.xml 	modified:   AppDropboxBq/bin/AndroidManifest.xml 	new file:   AppDropboxBq/bin/AppDropboxBq.apk 	new file:   AppDropboxBq/bin/classes.dex 	modified:   AppDropboxBq/bin/classes/com/example/appdropboxbq/MainActivity.class 	new file:   AppDropboxBq/bin/dexedLibs/android-support-v4-d1eeab404a21a9a329a88161490b012e.jar 	new file:   AppDropboxBq/bin/dexedLibs/dropbox-android-sdk-1.6-60d7040ea89abe9ddd31b7e975c62e54.jar 	new file:   AppDropboxBq/bin/dexedLibs/httpmime-4.0.3-e13f9476b000542289f55e460bc059fc.jar 	new file:   AppDropboxBq/bin/dexedLibs/json_simple-1.1-af8ac1618120db623f5cde6e80cffa2c.jar 	new file:   AppDropboxBq/bin/res/crunch/drawable-hdpi/ic_launcher.png 	new file:   AppDropboxBq/bin/res/crunch/drawable-mdpi/ic_launcher.png 	new file:   AppDropboxBq/bin/res/crunch/drawable-xhdpi/ic_launcher.png 	new file:   AppDropboxBq/bin/res/crunch/drawable-xxhdpi/ic_launcher.png 	new file:   AppDropboxBq/bin/resources.ap_ 	new file:   AppDropboxBq/libs/HTTPCOMPONENTS-LICENSE.txt 	new file:   AppDropboxBq/libs/JSON-SIMPLE-LICENSE.txt 	modified:   AppDropboxBq/libs/android-support-v4.jar 	new file:   AppDropboxBq/libs/dropbox-android-sdk-1.6.jar 	new file:   AppDropboxBq/libs/httpmime-4.0.3.jar 	new file:   AppDropboxBq/libs/json_simple-1.1.jar 	modified:   AppDropboxBq/src/com/example/appdropboxbq/MainActivity.java 	deleted:    AppDropboxBq/.settings/org.eclipse.jdt.core.prefs 	deleted:    AppDropboxBq/libs/armeabi/libDropboxSync.so 	deleted:    AppDropboxBq/libs/dropbox-sync-sdk-android.jar 	deleted:    AppDropboxBq/libs/mips/libDropboxSync.so 	deleted:    AppDropboxBq/libs/x86/libDropboxSync.so

--- AppDropboxBq/src/com/example/appdropboxbq/MainActivity.java
+++ AppDropboxBq/src/com/example/appdropboxbq/MainActivity.java
@@ -2,14 +2,60 @@ package com.example.appdropboxbq;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
+
+import java.io.File;
+
+import android.content.SharedPreferences.Editor;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.android.AuthActivity;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
+
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = "AppDropBox > MainActivity";
+	
+	final static private String APP_KEY = "ib668l9z4j4ps2a";
+    final static private String APP_SECRET = "w23by8h5dded9xn";
+
+    final static private String ACCOUNT_PREFS_NAME = "prefs";
+    final static private String ACCESS_KEY_NAME = "ACCESS_KEY";
+    final static private String ACCESS_SECRET_NAME = "ACCESS_SECRET";
+
+    private static final boolean USE_OAUTH1 = false;
+
+    DropboxAPI<AndroidAuthSession> mApi;
+
+    private boolean mLoggedIn;
+  
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       
+        AndroidAuthSession session = buildSession();
+        mApi = new DropboxAPI<AndroidAuthSession>(session);
+
         setContentView(R.layout.activity_main);
+        
+        if (mLoggedIn) {
+            logOut();
+        } else {
+            // Start the remote authentication
+            if (USE_OAUTH1) {
+                mApi.getSession().startAuthentication(MainActivity.this);
+            } else {
+                mApi.getSession().startOAuth2Authentication(MainActivity.this);
+            }
+        }
+        
     }
 
 
@@ -20,4 +66,97 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AndroidAuthSession session = mApi.getSession();
+
+        // The next part must be inserted in the onResume() method of the
+        // activity from which session.startAuthentication() was called, so
+        // that Dropbox authentication completes properly.
+        if (session.authenticationSuccessful()) {
+            try {
+                // Mandatory call to complete the auth
+                session.finishAuthentication();
+
+                // Store it locally in our app for later use
+                storeAuth(session);
+                mLoggedIn = true;
+            } catch (IllegalStateException e) {
+            	Toast error = Toast.makeText(this, "Couldn't authenticate with Dropbox:" + e.getLocalizedMessage(), Toast.LENGTH_LONG);
+                error.show();
+                Log.i(TAG, "Error authenticating", e);
+            }
+        }
+    }
+    
+    //////
+    
+    private AndroidAuthSession buildSession() {
+        AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
+
+        AndroidAuthSession session = new AndroidAuthSession(appKeyPair);
+        loadAuth(session);
+        return session;
+    }
+    
+    private void listBooks(){
+    
+    }
+    
+    private void loadAuth(AndroidAuthSession session) {
+        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+        String key = prefs.getString(ACCESS_KEY_NAME, null);
+        String secret = prefs.getString(ACCESS_SECRET_NAME, null);
+        if (key == null || secret == null || key.length() == 0 || secret.length() == 0) return;
+
+        if (key.equals("oauth2:")) {
+            // If the key is set to "oauth2:", then we can assume the token is for OAuth 2.
+            session.setOAuth2AccessToken(secret);
+        } else {
+            // Still support using old OAuth 1 tokens.
+            session.setAccessTokenPair(new AccessTokenPair(key, secret));
+        }
+    }
+    
+    private void storeAuth(AndroidAuthSession session) {
+        // Store the OAuth 2 access token, if there is one.
+        String oauth2AccessToken = session.getOAuth2AccessToken();
+        if (oauth2AccessToken != null) {
+            SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+            Editor edit = prefs.edit();
+            edit.putString(ACCESS_KEY_NAME, "oauth2:");
+            edit.putString(ACCESS_SECRET_NAME, oauth2AccessToken);
+            edit.commit();
+            return;
+        }
+        // Store the OAuth 1 access token, if there is one.  This is only necessary if
+        // you're still using OAuth 1.
+        AccessTokenPair oauth1AccessToken = session.getAccessTokenPair();
+        if (oauth1AccessToken != null) {
+            SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+            Editor edit = prefs.edit();
+            edit.putString(ACCESS_KEY_NAME, oauth1AccessToken.key);
+            edit.putString(ACCESS_SECRET_NAME, oauth1AccessToken.secret);
+            edit.commit();
+            return;
+        }
+    }
+    
+    private void logOut() {
+        // Remove credentials from the session
+        mApi.getSession().unlink();
+
+        // Clear our stored keys
+        clearKeys();
+        // Change UI state to display logged out version
+        mLoggedIn = false;
+    }
+    
+    private void clearKeys() {
+        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+        Editor edit = prefs.edit();
+        edit.clear();
+        edit.commit();
+    }
 }
